b091e16d37a17568f0077014092268b37b66dbf5
RicardoSe14 <Ricardo.s14@gmail.com>
2023-08-15 14:58:45 -0400

    segunda revisión

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get '/articles', to: "articles#index"
+
+
   # Defines the root path route ("/")
    root "articles#index"
 end
